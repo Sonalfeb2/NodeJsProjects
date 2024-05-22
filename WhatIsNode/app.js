9bd749401fbb14d78a2649bbe42b8cdc20a0576e
Sonal Agrawal <sonalfeb7@gmail.com>
2024-05-22 18:59:18 +0530

    Request/Response

--- WhatIsNode/app.js
+++ WhatIsNode/app.js
@@ -1,5 +1,12 @@
-const http = require('http');
-http.createServer((req,res)=>{
-    console.log(req)
-    res.end('Hello World!');
-}).listen(3000)
+const http = require("http");
+http
+  .createServer((req, res) => {
+    if (req.url == "/") {
+      res.write("<html>Welcome to my NodeJS Project</html>");
+    } else {
+      res.write(`<html><div>Welcome to my ${(req.url).slice(1)} Page</div></html>`);
+    }
+
+    res.end();
+  })
+  .listen(3000);

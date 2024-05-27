6b385a74aa99f177fbb7ce574c96b2f20620f175
Sonal Agrawal <sonalfeb7@gmail.com>
2024-05-27 16:20:46 +0530

    Writing and Reading From a File

--- WhatIsNode/app.js
+++ WhatIsNode/app.js
@@ -1,12 +1,33 @@
 const http = require("http");
+const fs = require('fs')
 http
   .createServer((req, res) => {
-    if (req.url == "/") {
-      res.write("<html>Welcome to my NodeJS Project</html>");
-    } else {
-      res.write(`<html><div>Welcome to my ${(req.url).slice(1)} Page</div></html>`);
-    }
+    
+    if(req.method==='POST' && req.url === "/message"){
+      const body = [];
+      req.on('data',(chunk)=>{
+        body.push(chunk)
+        
+        
+      })
+     return req.on('end',()=>{
+        const parseBody = Buffer.concat(body).toString();
+        const message = parseBody.split('=')[1]
+        fs.writeFileSync('message.txt',message )
+        res.statusCode = 302;
+        res.setHeader('Location','/')
+       return res.end();
 
+      })
+      
+      
+    }
+    
+   const readmessage =  fs.readFileSync('message.txt','utf8');
+    res.write('<html>')
+    res.write(`<div>${readmessage&&readmessage}</div>`)
+    res.write('<form action = "message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form>')
+    res.write('</html>')
     res.end();
   })
   .listen(3000);

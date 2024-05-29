f3856ece4a0f546b993c85683590ca694b9674d4
Sonal Agrawal <sonalfeb7@gmail.com>
2024-05-29 18:22:40 +0530

    Cleanup Code

--- WhatIsNode/app.js
+++ WhatIsNode/app.js
@@ -1,33 +1,4 @@
 const http = require("http");
-const fs = require('fs')
-http
-  .createServer((req, res) => {
-    
-    if(req.method==='POST' && req.url === "/message"){
-      const body = [];
-      req.on('data',(chunk)=>{
-        body.push(chunk)
-        
-        
-      })
-     return req.on('end',()=>{
-        const parseBody = Buffer.concat(body).toString();
-        const message = parseBody.split('=')[1]
-        fs.writeFileSync('message.txt',message )
-        res.statusCode = 302;
-        res.setHeader('Location','/')
-       return res.end();
-
-      })
-      
-      
-    }
-    
-   const readmessage =  fs.readFileSync('message.txt','utf8');
-    res.write('<html>')
-    res.write(`<div>${readmessage&&readmessage}</div>`)
-    res.write('<form action = "message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form>')
-    res.write('</html>')
-    res.end();
-  })
-  .listen(3000);
+const routes = require("./routes");
+console.log(routes.someText)
+http.createServer(routes.handler).listen(3000);
---
+++ WhatIsNode/routes.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const requestHandler = (req,res)=>{
+        
+    if(req.method==='POST' && req.url === "/message"){
+        const body = [];
+        req.on('data',(chunk)=>{
+          body.push(chunk)
+          
+          
+        })
+       return req.on('end',()=>{
+          const parseBody = Buffer.concat(body).toString();
+          const message = parseBody.split('=')[1]
+          fs.writeFileSync('message.txt',message )
+          res.statusCode = 302;
+          res.setHeader('Location','/')
+         return res.end();
+  
+        })
+        
+        
+      }
+      
+     const readmessage =  fs.readFileSync('message.txt','utf8');
+      res.write('<html>')
+      res.write(`<div>${readmessage&&readmessage}</div>`)
+      res.write('<form action = "message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form>')
+      res.write('</html>')
+      res.end();
+}
+
+// module.exports = requestHandler
+
+// module.exports = {
+//     handler : requestHandler,
+//     someText : 'Some Hard Code text'
+// }
+
+// module.exports.handler = requestHandler
+// module.exports.someText= 'Some Hard Code text'
+
+
+exports.handler = requestHandler
+exports.someText= 'Some Hard Code text'
+    

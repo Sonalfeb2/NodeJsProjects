a0e9aa019886ccacca3a0f7f4875d9c7fc34aa5d
Sonal Agrawal <sonalfeb7@gmail.com>
2024-07-15 20:04:04 +0530

    Added Form and Body-Parser

--- Express/app.js
+++ Express/app.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
-app.use((req, res, next) => {
-  console.log("I am in Middleware");
-  res.send("<h1>Sonal</h1>");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use((req, res, next) => {
+//   console.log("I am in Middleware");
+//   res.send("<h1>Sonal</h1>");
+//   next();
+
+// });
+// app.use((req, res, next) => {
+//   console.log("I am in another Middleware");
+
+// });
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"> Add Product : <input type="text" name="title"/> Size : <input type="number" name="size"/><button type="submit">Click Me </button></form>'
+  );
 });
-app.use((req, res, next) => {
-  console.log("I am in another Middleware");
+app.post("/product", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+app.use("/", (req, res) => {
+  res.send("Hello From Express");
 });
 app.listen(3000);

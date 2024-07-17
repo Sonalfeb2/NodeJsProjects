3bd96c8503905d4933c9c69a8398089826c8ba45
Sonal Agrawal <sonalfeb7@gmail.com>
2024-07-17 14:57:17 +0530

    Added Validations in Inputs

--- SimpleGroupChat/routes/login.js
+++ SimpleGroupChat/routes/login.js
@@ -12,28 +12,27 @@ router.post("/user", (req, res) => {
   res.redirect("/");
 });
 router.get("/", (req, res) => {
-  fs.readFileSync("message.txt", "utf8", (err, data) => {
-    if (err) {
-      res.send(
-        `<h1>No Chats</h1><form action="/"  method="POST" onsubmit="document.getElementById('username').value=localStorage.getItem('username')"><input type="text" name="title" /><input type="hidden" name="username" id="username" /><button type="submit">Send</button></form>`
-      );
-    } else {
-      res.send(
-        `<h1>${data}</h1><form action="/"  method="POST" onsubmit="document.getElementById('username').value=localStorage.getItem('username')"><input type="text" name="title" /><input type="hidden" name="username" id="username" /><button type="submit">Send</button></form>`
-      );
-    }
-  });
+  const data = fs.readFileSync("message.txt", "utf8");
+
+  res.send(
+    `<h2>${data.length>0?data:"No Messages"}</h2><form action="/"  method="POST" onsubmit="document.getElementById('username').value=localStorage.getItem('username')"><input type="text" name="title" /><input type="hidden" name="username" id="username" /><button type="submit">Send</button></form>`
+  );
 });
 router.post("/", (req, res) => {
   const message = req.body.title;
   const username = req.body.username;
-  fs.appendFile("message.txt", ` ${username}:${message} `, err => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("saved");
-    }
-  });
-  res.redirect("/");
+  if (username.length > 0) {
+    fs.appendFile("message.txt", ` ${username}:${message} `, err => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("saved");
+      }
+    });
+
+    res.redirect("/");
+  } else {
+    res.redirect("/login");
+  }
 });
 module.exports = router;

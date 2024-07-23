4d25944fd578ee044a50dfa0911fd71d5fa9a21d
Sonal Agrawal <sonalfeb7@gmail.com>
2024-07-23 16:35:04 +0530

    Added Products into a Cart

--- DynamicRoutes/controllers/shop.js
+++ DynamicRoutes/controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require("../models/product");
-
+const Cart = require("../models/cart")
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render("shop/product-list", {
@@ -36,7 +36,13 @@ exports.getCart = (req, res, next) => {
     pageTitle: "Your Cart"
   });
 };
-
+exports.addCart=(req,res,next)=>{
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect("/cart")
+}
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
     path: "/orders",
